eee5fab9664a9a4e072d7a5195bbe6421aeb8434
ShubhradipSarkar <97940737+ShubhradipSarkar@users.noreply.github.com>
2022-08-26 22:04:37 +0530

    Update index.js
    updated js a bit so that it works for category as well;

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function f1() {
     var str = document.getElementById("fname").value;
-
+    var str_category=document.getElementById("catname").value;
+    let str3 = "&category=";
+    
+    let str_category_addition = str3+str_category;
     let str2 = "https://script.google.com/macros/s/AKfycbxJMiE8kFsHuw52ZbInP5_yQxKOTzVpDbjiG0YOH498Zu-HMqb-KAn4OwKWcka3vZ1pew/exec?program_name=";
-    console.log(str2 + str);
+    console.log(str2 + str + str_category_addition);
 
-    window.location.href = (str2 + str);
+    window.location.href = (str2 + str + str_category_addition);
 
 
 
