122329d9beafa0dde19b4b7e356f65b3be5b81f4
ShubhradipSarkar <97940737+ShubhradipSarkar@users.noreply.github.com>
2022-08-26 22:12:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function f1() {
     var str = document.getElementById("fname").value;
-    var str_category=document.getElementById("bal").value;
+    var str_category=document.getElementById("lname").value;
     let str3 = "&category=";
     
     let str_category_addition = str3+str_category;
